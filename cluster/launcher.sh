90114c0fcdc1340f9790359327ab5a4e4a591032
martin-millon <martin.millon@epfl.ch>
2018-09-19 09:55:30 +0200

    launcher change

--- cluster/launcher.sh
+++ cluster/launcher.sh
@@ -5,24 +5,21 @@
 # set the simulation basename and the file you want to run
 
 
-export JOB_BASENAME="test_regor"
+export JOB_BASENAME="3a"
 export SERVER_NAME="regor"  # on Regor : 'r3', 'r4' ; otherwise 'deneb' or 'fidis'
 export QUEUE_NAME="r4"  # on Regor : 'r3', 'r4' ; on deneb "debug" or "serial"
 
-export OBJECT_NAME='HE0435b'
-export NUM_CORE=16
+export OBJECT_NAME='J1455'
+export NUM_CORE="16"
 max_node=1
 
 
-
-
 start_file="start_3a.slurm"
-job_name_csr=$OBJECT_NAME$start_file
+job_name_csr=$OBJECT_NAME$JOB_BASENAME
 
-echo "job basename = "$JOB_BASENAME
+echo "job name = "$job_name_csr
 echo "start file = "$start_file
 echo "max node = "$max_node
-echo "MCMC_PyCS : sending job as 'sbatch -J $job_name_csr -n $NUM_CORE (-N $max_node) (-p $QUEUE_NAME) $start_file'"
+echo "MCMC_PyCS : sending job as sbatch -J $job_name_csr -n $NUM_CORE (-N $max_node) (-p $QUEUE_NAME) $start_file"
 
-sbatch -J $job_name_csr -p $QUEUE_NAME -n $num_core -N $max_node $start_file
-
+sbatch -J $job_name_csr -p $QUEUE_NAME -n $NUM_CORE -N 1 $start_file
