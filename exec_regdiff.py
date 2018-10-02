8f8c9d4c13719475e3fdfee68d7e39870669a757
martin-millon <martin.millon@epfl.ch>
2018-10-02 10:48:56 +0200

    adding multi launch for regdiff

---
+++ exec_regdiff.py
@@ -0,0 +1,34 @@
+
+import dill as pkl
+import argparse as ap
+import pycs, os
+
+def main(pickle_path, is_copy, path):
+
+    os.chdir(path)
+    if is_copy :
+        simset_copy, lcs, simoptfct, kwargs, opts, tsrand = pkl.load(pickle_path)
+        pycs.sim.run.multirun(simset_copy, lcs, simoptfct, kwargs_optim=kwargs,
+                          optset=opts, tsrand=tsrand)
+    else :
+        simset_mock, lcs, simoptfct, kwargs, opts, tsrand = pkl.load(pickle_path)
+        pycs.sim.run.multirun(simset_mock, lcs, simoptfct, kwargs_optim=kwargs,
+                              optset=opts, tsrand=tsrand, keepopt=True)
+
+
+if __name__ == '__main__':
+    parser = ap.ArgumentParser(prog="python {}".format(os.path.basename(__file__)),
+                               description="",
+                               formatter_class=ap.RawTextHelpFormatter)
+    help_pickle_path = ""
+    help_is_copy = ""
+
+    parser.add_argument(dest='pickle_path', type=str,
+                        metavar='pickle_path', action='store',
+                        help=help_pickle_path)
+    parser.add_argument(dest='is_copy', type=bool,
+                        metavar='is_copy', action='store',
+                        help=help_is_copy)
+
+    args = parser.parse_args()
+    main(args.pickle_path,args.is_copy)
