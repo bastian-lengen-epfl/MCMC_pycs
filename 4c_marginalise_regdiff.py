3206084e60e316f83b72405ae2965b124b5e6f84
martin-millon <martin.millon@epfl.ch>
2018-10-03 12:15:15 +0200

    update the marginalisation of regdiff

--- 4c_marginalise_regdiff.py
+++ 4c_marginalise_regdiff.py
@@ -2,10 +2,11 @@ import sys
 import pycs
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle as pkl
 import importlib
 import argparse as ap
+from module import util_func as ut
+
 
 def main(lensname,dataname,work_dir='./'):
     sys.path.append(work_dir + "config/")
@@ -23,32 +24,82 @@ def main(lensname,dataname,work_dir='./'):
     if not os.path.isdir(marginalisation_dir):
         os.mkdir(marginalisation_dir)
 
-    f = open(marginalisation_dir + 'report_%s_sigma%2.1f.txt'%(config.name_marg_regdiff,config.sigmathresh), 'w')
-
     if config.testmode:
         nbins = 500
     else:
         nbins = 5000
 
-    colors = ["royalblue", "crimson", "seagreen", "darkorchid", "darkorange", 'indianred', 'purple', 'brown', 'black', 'violet', 'paleturquoise', 'palevioletred', 'olive',
-              'indianred', 'salmon','lightcoral', 'chocolate', 'indigo', 'steelblue' , 'cyan', 'gold']
-    color_id = 0
+    colors = ["royalblue", "crimson", "seagreen", "darkorchid", "darkorange", 'indianred', 'purple', 'brown', 'black',
+              'violet', 'paleturquoise', 'palevioletred', 'olive',
+              'indianred', 'salmon', 'lightcoral', 'chocolate', 'indigo', 'steelblue', 'cyan', 'gold']
 
-    group_list = []
-    medians_list = []
-    errors_up_list = []
-    errors_down_list = []
-    simset_mock_ava = ["mocks_n%it%i_%s" % (int(config.nsim * config.nsimpkls), config.truetsr,i) for i in config.tweakml_name_marg_regdiff]
-    opt = 'regdiff'
+    f = open(marginalisation_dir + 'report_%s_sigma%2.1f.txt' % (config.name_marg_regdiff, config.sigmathresh), 'w')
+    path_list = []
+    name_list = []
 
-    if config.auto_all :
-        pass
-        #TODO write this : true marginalisation over regdiff params + take the most precise estimate over the knotstep...
+    for a, kn in enumerate(config.knotstep_marg_regdiff):
+        for b, knml in enumerate(config.mlknotsteps_marg_regdiff):
 
-    else :
-        count = 0
-        for a,kn in enumerate(config.knotstep_marg_regdiff):
-            for b, knml in enumerate(config.mlknotsteps_marg_regdiff):
+
+            color_id = 0
+
+            group_list = []
+            medians_list = []
+            errors_up_list = []
+            errors_down_list = []
+            simset_mock_ava = ["mocks_n%it%i_%s" % (int(config.nsim * config.nsimpkls), config.truetsr,twk) for twk in config.tweakml_name_marg_regdiff]
+            opt = 'regdiff'
+
+            if config.auto_marginalisation:
+                if config.use_preselected_regdiff == False :
+                    raise RuntimeError("Turn the use_preselected_regdiff to True and set your preselection_file before using auto_marginalisation.")
+
+                kw_list = ut.read_preselected_regdiffparamskw(config.preselection_file)
+
+                for paramskw in kw_list :
+                    for n, noise in enumerate(config.tweakml_name_marg_regdiff):
+                        result_file_delay = config.lens_directory + config.combkw[a, b] + '/sims_%s_opt_%st%i/' \
+                                            % (config.simset_copy, paramskw, int(config.tsrand)) \
+                                            + 'sims_%s_opt_%s' % (config.simset_copy, paramskw) + 't%i_delays.pkl' % int(
+                            config.tsrand)
+                        result_file_errorbars = config.lens_directory + config.combkw[a, b] + '/sims_%s_opt_%st%i/' \
+                                                % (config.simset_copy, paramskw, int(config.tsrand)) \
+                                                + 'sims_%s_opt_%s' % (simset_mock_ava[n], paramskw) + \
+                                                't%i_errorbars.pkl' % int(config.tsrand)
+
+                        if not os.path.isfile(result_file_delay) or not os.path.isfile(result_file_errorbars):
+                            print 'Error I cannot find the files %s or %s. ' \
+                                  'Did you run the 3c and 4a?' % (result_file_delay, result_file_errorbars)
+                            f.write('Error I cannot find the files %s or %s. \n' % (
+                            result_file_delay, result_file_errorbars))
+                            continue
+
+                        group_list.append(pycs.mltd.comb.getresults(
+                            pycs.mltd.comb.CScontainer(data=dataname, knots=kn, ml=knml,
+                                                       name="set %i" % count,
+                                                       drawopt=config.optfctkw, runopt=opt,
+                                                       ncopy=config.ncopy * config.ncopypkls,
+                                                       nmocks=config.nsim * config.nsimpkls, truetsr=config.truetsr,
+                                                       colour=colors[color_id],
+                                                       result_file_delays=result_file_delay,
+                                                       result_file_errorbars=result_file_errorbars)))
+                        medians_list.append(group_list[-1].medians)
+                        errors_up_list.append(group_list[-1].errors_up)
+                        errors_down_list.append(group_list[-1].errors_down)
+                        color_id += 1
+                        if color_id >= len(colors):
+                            print "Warning : I don't have enough colors in my list, I'll restart from the beginning."
+                            color_id = 0  # reset the color form the beginning
+
+                        f.write('Set %i, knotstep : %2.2f, mlknotstep : %2.2f \n' % (count, kn, knml))
+                        f.write('covkernel : %s, point density: %2.2f, pow : %2.2f, amp : %2.2f, '
+                                'scale:%2.2f, errscale:%2.2f \n' % (c, pts, p, am, s, e))
+                        f.write('Tweak ml name : %s \n' % noise)
+                        f.write('------------------------------------------------ \n')
+
+
+            else :
+                count = 0
                 for c in config.covkernel_marg:
                     for pts in config.pointdensity_marg:
                         for p in config.pow_marg:
@@ -98,48 +149,72 @@ def main(lensname,dataname,work_dir='./'):
                                             f.write('Tweak ml name : %s \n'%noise)
                                             f.write('------------------------------------------------ \n')
 
+            #build the bin list :
+            medians_list = np.asarray(medians_list)
+            errors_down_list = np.asarray(errors_down_list)
+            errors_up_list = np.asarray(errors_up_list)
+            binslist = []
+            for i, lab in enumerate(config.delay_labels):
+                bins = np.linspace(min(medians_list[:,i]) - 10 *min(errors_down_list[:,i]), max(medians_list[:,i]) + 10*max(errors_up_list[:,i]), nbins)
+                binslist.append(bins)
 
 
-    #build the bin list :
-    medians_list = np.asarray(medians_list)
-    errors_down_list = np.asarray(errors_down_list)
-    errors_up_list = np.asarray(errors_up_list)
-    binslist = []
-    for i, lab in enumerate(config.delay_labels):
-        bins = np.linspace(min(medians_list[:,i]) - 10 *min(errors_down_list[:,i]), max(medians_list[:,i]) + 10*max(errors_up_list[:,i]), nbins)
-        binslist.append(bins)
+            color_id = 0
+            for g,group in enumerate(group_list):
+                group.binslist = binslist
+                group.plotcolor = colors[color_id]
+                group.linearize(testmode=config.testmode)
+                color_id += 1
+                if color_id >= len(colors):
+                    print "Warning : I don't have enough colors in my list, I'll restart from the beginning."
+                    color_id = 0  # reset the color form the beginning
+
+            combined= pycs.mltd.comb.combine_estimates(group_list, sigmathresh=config.sigmathresh, testmode=config.testmode)
+            combined.linearize(testmode=config.testmode)
+            combined.name = 'combined $\sigma = %2.2f$'%config.sigmathresh
+
+            print "%s : Combination of the regdiff parameters for spline parameters kn = %i knml = %i"%(config.name_marg_regdiff, kn,knml)
+            combined.niceprint()
+
+            #plot the results :
+
+            text = [
+                (0.75, 0.92, r"$\mathrm{"+config.full_lensname+"}$" + "\n" + r"$\mathrm{PyCS\ estimates}$",
+                 {"fontsize": 26, "horizontalalignment": "center"})]
+
+            if config.display :
+                pycs.mltd.plot.delayplot(group_list+[combined], rplot=8.0, refgroup=combined,
+                                         text=text, hidedetails=True, showbias=False, showran=False,
+                                         showlegend=True, figsize=(15, 10), horizontaldisplay=False, legendfromrefgroup=False)
+
+            pycs.mltd.plot.delayplot(group_list+[combined], rplot=8.0, refgroup=combined, text=text, hidedetails=True,
+                                     showbias=False, showran=False, showlegend=True, figsize=(15, 10), horizontaldisplay=False,
+                                     legendfromrefgroup=False, filename = indiv_marg_dir + config.name_marg_regdiff + "kn%i-knml%i_sigma_%2.2f.png"%(kn,knml,config.sigmathresh))
+
+            pkl.dump(group_list, open(marginalisation_dir + config.name_marg_regdiff +"kn%i-knml%i_sigma_%2.2f"%(kn,knml,config.sigmathresh) +'_goups.pkl', 'wb'))
+            pkl.dump(combined, open(marginalisation_dir + config.name_marg_regdiff + "kn%i-knml%i_sigma_%2.2f"%(kn,knml,config.sigmathresh) + '_combined.pkl', 'wb'))
+            path_list.append(marginalisation_dir + config.name_marg_regdiff + "kn%i-knml%i_sigma_%2.2f"%(kn,knml,config.sigmathresh) + '_combined.pkl')
+            name_list.append('Regdiff, kn : %i, knml : %i'%(kn,knml))
 
 
-    color_id = 0
-    for g,group in enumerate(group_list):
-        group.binslist = binslist
-        group.plotcolor = colors[color_id]
-        group.linearize(testmode=config.testmode)
-        color_id += 1
-        if color_id >= len(colors):
-            print "Warning : I don't have enough colors in my list, I'll restart from the beginning."
-            color_id = 0  # reset the color form the beginning
-
-    combined= pycs.mltd.comb.combine_estimates(group_list, sigmathresh=config.sigmathresh, testmode=config.testmode)
-    combined.linearize(testmode=config.testmode)
-    combined.name = 'combined $\sigma = %2.2f$'%config.sigmathresh
-
-    print "Final combination for marginalisation ", config.name_marg_regdiff
-    combined.niceprint()
-
-    #plot the results :
-
+    ###################  MAKE THE FINAL REGDIFF ESTIMATE ####################
+    final_groups, final_combined = ut.group_estimate(path_list, name_list, config.delay_labels, colors, config.sigma_thresh, config.name_marg_regdiff, testmode = config.testmode)
     text = [
-        (0.75, 0.92, r"$\mathrm{"+config.full_lensname+"}$" + "\n" + r"$\mathrm{PyCS\ estimates}$",
+        (0.75, 0.92, r"$\mathrm{" + config.full_lensname + "}$" + "\n" + r"$\mathrm{PyCS\ estimates}$",
          {"fontsize": 26, "horizontalalignment": "center"})]
 
-    if config.display :
-        pycs.mltd.plot.delayplot(group_list+[combined], rplot=8.0, refgroup=combined, text=text, hidedetails=True, showbias=False, showran=False, showlegend=True, figsize=(15, 10), horizontaldisplay=False, legendfromrefgroup=False)
+    if config.display:
+        pycs.mltd.plot.delayplot(final_groups + [final_combined], rplot=8.0, refgroup=final_combined,
+                                 text=text, hidedetails=True, showbias=False, showran=False,
+                                 showlegend=True, figsize=(15, 10), horizontaldisplay=False, legendfromrefgroup=False)
 
-    pycs.mltd.plot.delayplot(group_list+[combined], rplot=8.0, refgroup=combined, text=text, hidedetails=True, showbias=False, showran=False, showlegend=True, figsize=(15, 10), horizontaldisplay=False, legendfromrefgroup=False, filename = indiv_marg_dir + config.name_marg_regdiff +"_sigma_%2.2f.png"%config.sigmathresh)
+    pycs.mltd.plot.delayplot(final_groups + [final_combined], rplot=8.0, refgroup=final_combined, text=text, hidedetails=True,
+                             showbias=False, showran=False, showlegend=True, figsize=(15, 10), horizontaldisplay=False,
+                             legendfromrefgroup=False,
+                             filename=indiv_marg_dir + config.name_marg_regdiff + "_final_sigma_%2.2f.png" % (config.sigmathresh))
 
-    pkl.dump(group_list, open(marginalisation_dir + config.name_marg_regdiff +"_sigma_%2.2f"%config.sigmathresh +'_goups.pkl', 'wb'))
-    pkl.dump(combined, open(marginalisation_dir + config.name_marg_regdiff + "_sigma_%2.2f"%config.sigmathresh + '_combined.pkl', 'wb'))
+    pkl.dump(group_list, open(marginalisation_dir + config.name_marg_regdiff + "_final_sigma_%2.2f" % (config.sigmathresh) + '_goups.pkl', 'wb'))
+    pkl.dump(combined, open(marginalisation_dir + config.name_marg_regdiff + "_final_sigma_%2.2f" % (config.sigmathresh) + '_combined.pkl', 'wb'))
 
 
 if __name__ == '__main__':
--- config_default_quads.py
+++ config_default_quads.py
@@ -106,9 +106,11 @@ mlknotsteps_marg = mlknotsteps
 # Chose the parameters you want to marginalise on for the regdiff optimiser. Script 4c.
 name_marg_regdiff = 'marginalisation_regdiff'
 tweakml_name_marg_regdiff = ['PS']
-auto_all = False #set this flag to True (recommanded) and it will use all the available regdiff simulation (all of the line below are ignored)
-knotstep_marg_regdiff = knotstep
-mlknotsteps_marg_regdiff = mlknotsteps
+knotstep_marg_regdiff = knotstep #choose the knotstep range you want to marginalise over, by default it is recommanded to take the same as knotstep
+mlknotsteps_marg_regdiff = mlknotsteps #choose the mlknotstep range you want to marginalise over, by default it is recommanded to take the same as mlknotstep
+auto_marginalisation = True #set this flag to True (recommanded) and it will marginalise over the setting in your preselection_file.
+
+# if you don't want to use the auto marginalisation
 covkernel_marg = covkernel #parameters to marginalise over, give a list or just select the same that you used above to marginalise over all the available parameters
 pointdensity_marg = pointdensity
 pow_marg = pow
@@ -117,14 +119,15 @@ scale_marg = scale
 errscale_marg = errscale
 
 #other parameteres for regdiff and spline marginalisation :
-testmode = True
+testmode = True #Number of bins to use for the marginalisation, True is faster and usually precise enough
 delay_labels = ["AB", "AC", "AD", "BC", "BD" , "CD"]
-sigmathresh = 0   #sigma threshold for sigma clipping, 0 is a true marginalisation, choose 1000 to take the most precise.
+sigmathresh = 0.5   #sigma threshold for sigma clipping, 0 is a true marginalisation, choose 1000 to take the most precise.
 
 ###### MARGGINALISE SPLINE AND REGDIFF TOGETHER #######
 #choose here the marginalisation you want to combine in script 4d, it will also use the sigmathresh:
 name_marg_list = ['marginalisation_1','marginalisation_2']
 new_name_marg = 'marg_12'
+sigmathresh_list = [0.5,0.5] #sigmathresh used for marginalisation_1 and marginalisation_2, it can be different from the sigmathresh used for the new marginalisation
 
 
 ### Functions definition
