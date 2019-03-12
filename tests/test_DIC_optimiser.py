0d80fec51c6b71977798c49d029e1f0bda5cc34e
martin-millon <martin.millon@epfl.ch>
2019-03-12 15:48:59 -0400

    adding error handling in the DIC optimiser

--- tests/test_DIC_optimiser.py
+++ tests/test_DIC_optimiser.py
@@ -9,10 +9,10 @@ import numpy as np
 from module.optimisation import Optimiser as mcmc
 
 source ="pickle"
-object = "WFI2033"
+object = "WG0214"
 dataname="WFI"
 
-kntstp = 35
+kntstp = 15
 ml_kntstep =50
 
 picklepath = "../Simulation/" + object +'_'+dataname + '/' + 'spl1_ks%i_splml_ksml_%i/'%(kntstp, ml_kntstep)
@@ -20,14 +20,15 @@ picklename ="initopt_%s_ks%i_ksml%i.pkl"%(dataname, kntstp, ml_kntstep)
 
 (lcs, spline) = pycs.gen.util.readpickle(picklepath + picklename)
 fit_vector = mcmc.get_fit_vector(lcs, spline)
+pycs.sim.draw.saveresiduals(lcs, spline)
 
 dic_opt = mcmc.Dic_Optimiser(lcs, fit_vector, spline, knotstep=kntstp,
-                             savedirectory='./test_DIC',
+                             savedirectory='./test_DIC/',
                              recompute_spline=True, max_core=8,
-                             n_curve_stat=4,
-                             shotnoise=None, tweakml_type=config.tweakml_type,
-                             tweakml_name=config.tweakml_name, display=config.display, verbose=False,
-                             correction_PS_residuals=True, max_iter=config.max_iter)
+                             n_curve_stat=8,
+                             shotnoise=None, tweakml_type='PS_from_residuals',
+                             tweakml_name='PS', display=False, verbose=False,
+                             correction_PS_residuals=True, max_iter=4)
 
 chain = dic_opt.optimise()
 dic_opt.analyse_plot_results()
